999da899ad0523e295da37197b75bf68476e4379
Wagner Paulino <wagnersantos9778@gmail.com>
2021-01-12 10:15:47 -0300

    conexão com rabbit

--- service-1/src/main.ts
+++ service-1/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import { QUEUE_NAME } from './config/config-server';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://admin:admin@localhost:5672'],
+      queue: QUEUE_NAME,
+      queueOptions: {
+        durable: false
+      },
+    },
+  });
+  app.listen(() => console.log('Microservice is listening'));
 }
 bootstrap();
+
