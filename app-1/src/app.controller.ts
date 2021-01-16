615f2dba158f22686880d12cdeae2a5df72de85d
Wagner Paulino <wagnersantos9778@gmail.com>
2021-01-16 18:34:27 -0300

    exemplo com swagger

--- app-1/src/app.controller.ts
+++ app-1/src/app.controller.ts
@@ -2,12 +2,14 @@ import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ClientProxy } from '@nestjs/microservices';
 import { EXCHANGE_NAME } from './config/config-server';
+import { ApiResponse } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private appService: AppService, @Inject('POST_MESSAGES') private clientProxy: ClientProxy) { }
 
   @Get()
+  @ApiResponse({ status: 200, description: 'The Hello has been successfully created.' })
   async getHello(): Promise<string> {
     await this.clientProxy.emit(EXCHANGE_NAME, 'hello').toPromise();
     return this.appService.getHello();
--- app-1/src/main.ts
+++ app-1/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Posts example')
+    .setDescription('The Post API description')
+    .setVersion('1.0')
+    .addTag('posts')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
