9f1eaaac7910cb9416fc8c66c77ee2fd03204368
Wagner Paulino <wagnersantos9778@gmail.com>
2021-01-16 13:34:11 -0300

    message pattern

--- service-1/src/app.controller.ts
+++ service-1/src/app.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { MessagePattern, Payload, Ctx, RmqContext } from '@nestjs/microservices';
+import { EXCHANGE_NAME } from './config/config-server';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @MessagePattern(EXCHANGE_NAME)
+  getNotifications(@Payload() data: number[], @Ctx() context: RmqContext) {
+    console.log(`Pattern: ${context.getPattern()}`);
   }
+  
 }
