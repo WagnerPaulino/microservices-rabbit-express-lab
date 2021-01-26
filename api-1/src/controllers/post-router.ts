1caecce149ea54ba39c1662b8444f4e8269c7615
Wagner Paulino dos Santos <wagnersantos@inatel.br>
2021-01-26 16:02:01 -0300

    inclusão do swagger

--- api-1/src/controllers/post-router.ts
+++ api-1/src/controllers/post-router.ts
@@ -4,30 +4,60 @@ import RabbitmqServer from "../rabbitmq/rabbitmq-server";
 import { getRepository } from "typeorm";
 import { Post } from "../domain/post";
 
-const baseUrl = '/posts'
+const baseUrl = "/posts";
 
 const postRouter = Router();
 
 let rabbit: RabbitmqServer;
 
 const initRabbit = async () => {
-    if (rabbit) {
-        return;
-    }
-    rabbit = new RabbitmqServer("amqp://admin:admin@localhost:5672");
-    await rabbit.start();
-}
+  if (rabbit) {
+    return;
+  }
+  rabbit = new RabbitmqServer("amqp://admin:admin@localhost:5672");
+  await rabbit.start();
+};
 
+/**
+ * @swagger
+ * /api/posts:
+ *   get:
+ *     tags:
+ *       - Post
+ *     description: Returns all Posts
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: An array of Posts
+ */
 postRouter.get(baseUrl, async (_req: Request, res: Response) => {
-    const repository = getRepository(Post);
-    return res.send(await repository.find());
-})
+  const repository = getRepository(Post);
+  return res.send(await repository.find());
+});
 
+/**
+ * @swagger
+ * /api/posts:
+ *   post:
+ *     tags:
+ *       - Post
+ *     description: Returns all puppies
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Save a Post and return it
+ */
 postRouter.post(baseUrl, async (req: Request, res: Response) => {
-    await initRabbit();
-    await rabbit.publishInExchange(EXCHANGE_NAME, QUEUE_NAME, JSON.stringify(req.body));
-    const repository = getRepository(Post);
-    return res.send(await repository.save(req.body));
-})
+  await initRabbit();
+  await rabbit.publishInExchange(
+    EXCHANGE_NAME,
+    QUEUE_NAME,
+    JSON.stringify(req.body)
+  );
+  const repository = getRepository(Post);
+  return res.send(await repository.save(req.body));
+});
 
 export default postRouter;
--- api-1/src/index.ts
+++ api-1/src/index.ts
@@ -3,17 +3,34 @@ import bodyParser from "body-parser";
 import helmet from "helmet";
 import "reflect-metadata";
 import { createConnection } from "typeorm";
-import postRouter from './controllers/post-router';
+import postRouter from "./controllers/post-router";
+import swaggerJSDoc, { Options } from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+
 const PORT = 3000;
 const app = express();
 
-const init = createConnection().then(async _connection => {
-    app.use(helmet());
-    app.use(bodyParser.json());
-    app.use('/api', [postRouter]);
-    const server = app.listen(PORT);
-    console.log("Up in " + PORT);
-    return { app, server };
+const swaggerOptions: Options = {
+  swaggerDefinition: {
+    info: {
+      title: "microservices-rabbit-express-lab",
+      version: "1.0.0",
+    },
+    host: "localhost:3000",
+    basePath: "/",
+  },
+  apis: ["**/*-router.ts"],
+};
+var swaggerSpec = swaggerJSDoc(swaggerOptions);
+
+const init = createConnection().then(async (_connection) => {
+  app.use(helmet());
+  app.use(bodyParser.json());
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.use("/api", [postRouter]);
+  const server = app.listen(PORT);
+  console.log("Up in " + PORT);
+  return { app, server };
 });
 
-export default init
+export default init;
